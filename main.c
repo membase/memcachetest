1bb1e1792b43e2c818dc34e0e27c2d9e116e0eb3
Sarath Lakshman <sarathlakshman@slynux.com>
2011-12-04 17:04:13 +0530

    Added -T time_limit option, which can be used along with -l option to specify the test execution time limit
    This option mimics the -t option for repeated test facility is memcached test program brutis. -T time_in_seconds should be used along with -l repeat and loop option.
    Summary of total sets, gets, time taken for test and ops/sec are also displayed in loop mode when program is terminated by using SIGINT or when program executes for given time, specified by -T.

--- main.c
+++ main.c
@@ -354,6 +354,7 @@ static void release_memcached_handle(void *handle) {
 static inline int memcached_set_wrapper(struct connection *connection,
                                         const char *key, int nkey,
                                         const void *data, int size) {
+
     struct memcachelib* lib = (struct memcachelib*)connection->handle;
     switch (lib->type) {
 #ifdef HAVE_LIBMEMCACHED
@@ -404,6 +405,7 @@ static inline int memcached_set_wrapper(struct connection *connection,
     default:
         abort();
     }
+    sets_count++;
     return 0;
 }
 
@@ -479,6 +481,7 @@ static inline bool memcached_get_wrapper(struct connection* connection,
         abort();
     }
 
+    gets_count++;
     return true;
 }
 
@@ -614,7 +617,9 @@ static int populate_dataset(struct thread_context *ctx) {
     size_t nkey;
     int sres = -1;
 
-    assert(end > ctx->offset);
+    if (end > ctx->offset)
+	ctx->offset = 0;
+
     if (verbose) {
         fprintf(stderr, "Populating from %d to %d\n", ctx->offset, end);
     }
@@ -882,6 +887,15 @@ static int get_server_rusage(const struct host *entry, struct rusage *rusage) {
     return ret;
 }
 
+static void exit_handler(int signum)
+{
+    int end_time = (int) time(NULL);
+    int time_taken = end_time - start_time;
+    int ops_sec = ((float) gets_count+sets_count)/time_taken;
+    printf("\n\ngets: %d, sets: %d, time: %d, ops/sec: %d\n", gets_count, sets_count, time_taken, ops_sec);
+    exit(0);
+}
+
 /**
  * Program entry point
  * @param argc argument count
@@ -911,6 +925,7 @@ int main(int argc, char **argv) {
         case 'p':
             progress = 1;
             break;
+
         case 'P':
             setprc = atoi(optarg);
             if (setprc > 100) {
@@ -937,6 +952,8 @@ int main(int argc, char **argv) {
             break;
         case 'h': add_host(optarg);
             break;
+	case 'T': runtime_limit = atoll(optarg);
+	    break;
         case 'i': no_items = atoi(optarg);
             break;
         case 's': srand(atoi(optarg));
@@ -984,6 +1001,7 @@ int main(int argc, char **argv) {
             fprintf(stderr, "\t-h The hostname:port where the memcached server is running\n");
             fprintf(stderr, "\t   (use mulitple -h args for multiple servers)\n");
             fprintf(stderr, "\t-t The number of threads to use\n");
+            fprintf(stderr, "\t-T The number of seconds for which test is to be carried out (used with -l loop and repeat)\n");
             fprintf(stderr, "\t-i The number of items to operate with\n");
             fprintf(stderr, "\t-c The number of iteratons each thread should do\n");
             fprintf(stderr, "\t-l Loop and repeat the test, but print out information for each run\n");
@@ -1054,7 +1072,16 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Failed to get server stats\n");
     }
 
-
+    if (loop)
+    {
+        signal(SIGALRM, exit_handler);
+        signal(SIGINT, exit_handler);
+        start_time = (int) time (NULL);
+        if (runtime_limit > 0)
+        {
+	    alarm(runtime_limit);
+        }
+    }
     size_t nget = 0;
     size_t nset = populate ? no_items : 0;
     do {
